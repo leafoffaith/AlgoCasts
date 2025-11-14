9b58f71525c36e0fc331e66a93fa742f1971eb8c
Shaurya <shaurya26@gmail.com>
2025-11-14 22:44:01 +0530

    Update index.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) { }
+function reverse(str) {
+    let reversed = []
+    //abc = cba, abcd = dcba
+    //get length
+    let length = str.length;
+    // console.log(length)
+    // return reversed;
+    if (length % 2 !== 0) {
+        console.log("this is running for odd")
+        y = length - 1
+        reversed = str.split("")
+        console.log(str, "main str")
+        for (let i = 0; i < Math.ceil(length / 2); i++) {
+            reversed[y] = str[i];
+            reversed[i] = str[y];
+            y -= 1;
+            console.log(i)
+            console.log(reversed)
+        }
+    } else {
+        y = length - 1
+        reversed = str.split("")
+        for (let i = 0; i < (length / 2); i++) {
+            temp = reversed[y]
+            reversed[y] = reversed[i]
+            reversed[i] = temp
+            y -= 1
+        }
+    }
+    return reversed.join("");
+}
 
 module.exports = reverse;
